fd6edc2f89ef0895fb602b192705a0affd3b0217
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-03-09 15:53:45 -0500

    only use one client

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use rand::Fill;
-use std::{sync::atomic::AtomicU32, time::Duration};
+use std::{
+    sync::{atomic::AtomicU32, Arc},
+    time::Duration,
+};
 
 use creditcoin_runtime::runtime_types::{
     frame_support::storage::bounded_vec::BoundedVec, pallet_creditcoin::types::Blockchain,
 };
-use goose::prelude::*;
+use goose::{
+    metrics::{GooseRawRequest, GooseRequestMetric},
+    prelude::*,
+};
+use rand::distributions::{Alphanumeric, DistString};
 use sp_keyring::AccountKeyring;
 use subxt::{ClientBuilder, DefaultConfig, DefaultExtra, PairSigner};
 
@@ -17,49 +23,75 @@ static NONCE: AtomicU32 = AtomicU32::new(1);
 
 type RuntimeApi = creditcoin_runtime::RuntimeApi<DefaultConfig, DefaultExtra<DefaultConfig>>;
 
-async fn loadtest_register_address(user: &mut GooseUser) -> GooseTaskResult {
-    let api = user.get_session_data_unchecked::<RuntimeApi>();
+async fn loadtest_register_address(api: &RuntimeApi, user: &mut GooseUser) -> GooseTaskResult {
     let mut signer = PairSigner::new(AccountKeyring::Alice.pair());
-
-    let mut addr = [0u8; 16];
-    addr.try_fill(&mut rand::thread_rng()).unwrap();
     signer.set_nonce(NONCE.fetch_add(1, std::sync::atomic::Ordering::AcqRel));
-    if let Err(e) = api
-        .tx()
+
+    let addr = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+
+    api.tx()
         .creditcoin()
-        .register_address(Blockchain::Ethereum, BoundedVec(addr.to_vec()))
+        .register_address(Blockchain::Ethereum, BoundedVec(addr.into_bytes()))
         .sign_and_submit(&signer)
         .await
-    {
-        eprintln!("Error: {}", e);
-        drop(api);
-        init_client(user).await?;
-    }
-    Ok(())
-}
-
-async fn init_client(user: &mut GooseUser) -> GooseTaskResult {
-    let api = ClientBuilder::new().set_url("wss://qa.creditcoin.network:443").build().await.unwrap().to_runtime_api::<creditcoin_runtime::RuntimeApi<DefaultConfig, DefaultExtra<DefaultConfig>>>();
-    user.set_session_data(api);
+        .map_err(|e| {
+            eprintln!("rpc error: {}", e);
+            GooseTaskError::RequestFailed {
+                raw_request: GooseRequestMetric {
+                    elapsed: 0 as u64,
+                    raw: GooseRawRequest {
+                        method: GooseMethod::Put,
+                        url: "wss://qa.creditcoin.network:443".into(),
+                        headers: vec![],
+                        body: String::new(),
+                    },
+                    name: "RegisterAddress".to_string(),
+                    final_url: "".to_string(),
+                    redirected: false,
+                    response_time: 0,
+                    status_code: 0,
+                    success: false,
+                    update: false,
+                    user: user.weighted_users_index,
+                    error: "".to_string(),
+                    coordinated_omission_elapsed: 0,
+                    user_cadence: 0,
+                },
+            }
+        })?;
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), GooseError> {
-    let api = ClientBuilder::new().set_url("wss://qa.creditcoin.network:443").build().await.unwrap().to_runtime_api::<creditcoin_runtime::RuntimeApi<DefaultConfig, DefaultExtra<DefaultConfig>>>();
+    let api = Arc::new(
+        ClientBuilder::new()
+            .set_url("wss://qa.creditcoin.network:443")
+            .build()
+            .await
+            .unwrap()
+            .to_runtime_api::<RuntimeApi>(),
+    );
     let init_nonce = api
         .client
         .fetch_nonce::<DefaultAccountData>(&AccountKeyring::Alice.to_account_id())
         .await
         .unwrap();
+
     NONCE.store(init_nonce, std::sync::atomic::Ordering::Relaxed);
+
+    let closure: GooseTaskFunction = Arc::new(move |user| {
+        let api = api.clone();
+        Box::pin(async move { loadtest_register_address(&api, user).await })
+    });
+
+    let task = GooseTask::new(closure);
+    let taskset = taskset!("LoadTestTasks")
+        .set_wait_time(Duration::from_secs(1), Duration::from_secs(2))?
+        .register_task(task);
+
     GooseAttack::initialize()?
-        .register_taskset(
-            taskset!("LoadTestTasks")
-                .set_wait_time(Duration::from_secs(0), Duration::from_secs(1))?
-                .register_task(task!(init_client).set_on_start().set_name("Client init"))
-                .register_task(task!(loadtest_register_address).set_name("RegisterAddress")),
-        )
+        .register_taskset(taskset)
         .execute()
         .await?
         .print();
